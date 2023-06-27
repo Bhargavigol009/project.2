cd4337c636daddb6232e298b9de43c63e884ab39
bhargavi <bhargavigol97@gmail.com>
2023-06-27 18:04:23 +0530

    first master

---
+++ PR_2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int number;
+clrscr();
+printf("enter any number: ");
+scanf("%d",&number);
+ (number%5==0)?printf("this number is even...")
+	      :printf("this number is odd....");
+
+
+getch();
+}
